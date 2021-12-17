78f590031b75632904df98b99941e94910a7dfa5
DarkCodepch <Aptech2351066@gmail.com>
2021-12-17 23:33:16 +0500

    Assignment on if condition assignment 02 updated

--- Assignment-01-Password-Checker/app.js
+++ Assignment-01-Password-Checker/app.js
@@ -3,12 +3,20 @@ let CurrentPassword = "Kamran";
 
 function Checkpassword(){
     let UserInput = document.getElementById('password').value
-    if(UserInput === CurrentPassword){
-        alert("Password matched")
+    if(UserInput === ''){
+        alert('Please Type something')
         document.getElementById("myForm").reset();
     }
+
     else{
-        alert("Please type correct password");
-        document.getElementById("myForm").reset();
+        if(UserInput === CurrentPassword){
+            alert("Password matched")
+            document.getElementById("myForm").reset();
+        }
+        else{
+            alert("Please type correct password");
+            document.getElementById("myForm").reset();
+        }
     }
+    
 }
