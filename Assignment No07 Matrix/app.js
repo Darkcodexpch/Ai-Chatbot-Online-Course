2b32bb13d5907465fde629834580aaf59e1fc87a
Darkcodexpch <ikamihere@gmail.com>
2022-02-01 14:27:35 +0500

    Matrixx Assignment via foor loop and arraymap

---
+++ Assignment No07 Matrix/app.js
@@ -0,0 +1,29 @@
+// let arr = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ]
+// let arr1 = [
+//     [9, 8, 7],
+//     [6, 5, 4],
+//     [3, 2, 1]
+// ]
+// let result;
+// // Done with for loop
+
+// // for (let i = 0; i < arr.length; i++) {
+// //     for (let j = 0; j < arr[i].length; j++) //arr[i].lenght yeh islye kara taky loop i k lenght tk chale like i m 4 5 index bh hun tw wo reach kre
+// //     {
+// //         let result = arr[i][j] * arr1[i][j];
+// //         console.log(result)
+// //     }
+
+// // }
+
+// //  through Map
+// arr.map((eachrow, i) => {
+//     eachrow.map((eachelement, j) => {
+//         result = arr[i][j] * arr1[i][j]
+//         console.log(result)
+//     })
+// })
